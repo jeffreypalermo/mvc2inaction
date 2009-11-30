681a5410bbce6425694ad79e082baa4f93b53231
Jimmy Bogard <jimmy.bogard@gmail.com>
2009-11-29 21:55:56 -0600

    Initial add of files for View sample

---
+++ src/Chapter01/AccountProfile/Controllers/ProfileController.cs
@@ -0,0 +1,17 @@
+using System.Web.Mvc;
+
+namespace AccountProfile.Controllers
+{
+	public class ProfileController : Controller
+	{
+		public ViewResult Index()
+		{
+			return View();
+		}
+
+		public ViewResult Edit()
+		{
+			return View();
+		}
+	}
+}
---
+++ src/Chapter01/AccountProfile/Models/ProfileModels.cs
@@ -0,0 +1,9 @@
+namespace AccountProfile.Models
+{
+	public class Profile
+	{
+		public string Username { get; set; }
+		public string FirstName { get; set; }
+		public string LastName { get; set; }
+	}
+}
