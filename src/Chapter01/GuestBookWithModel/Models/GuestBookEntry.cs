828662cabffd22a8dc13fccfc5d1f12a9beb8936
Ben Scheirman <ben@scheirman.com>
2009-11-30 20:57:45 -0600

    added GuestBookWithModel sample

---
+++ src/Chapter01/GuestBookWithModel/Models/GuestBookEntry.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GuestBookWithModel.Models
+{
+    public class GuestBookEntry
+    {
+        [Required]
+        public string Name { get; set; }
+
+        [Required]        
+        [RegularExpression(@"\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}\b", ErrorMessage = "Please enter a valid email address.")]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [Required, StringLength(30), DataType(DataType.MultilineText)]
+        public string Comments { get; set; }
+    }
+}
