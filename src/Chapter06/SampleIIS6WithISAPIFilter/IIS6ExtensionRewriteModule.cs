57cf384d8dcf2ee048c51e163b24d22ea418ee17
Ben Scheirman <ben@scheirman.com>
2009-12-06 21:03:43 -0600

    added 2 more samples for chapter 6

---
+++ src/Chapter06/SampleIIS6WithISAPIFilter/IIS6ExtensionRewriteModule.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Web;
+
+namespace SampleIIS6WithISAPIFilter
+{
+    public class IIS6ExtensionRewriteModule : IHttpModule
+    {
+        public void Dispose()
+        {
+        }
+
+        public void Init(HttpApplication context)
+        {
+            context.BeginRequest += context_BeginRequest;
+        }
+
+        void context_BeginRequest(object sender, EventArgs e)
+        {
+            string url = "~" + HttpContext.Current.Request.Url.PathAndQuery;
+            if (url.Contains(".mvc"))
+            {
+                string newUrl = url.Replace(".mvc", "");
+                HttpContext.Current.RewritePath(newUrl);
+            }
+        }
+    }
+}
---
+++ src/Chapter06/SampleIIS7Hosting/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SampleIIS7Hosting.MvcApplication" Language="C#" %>
