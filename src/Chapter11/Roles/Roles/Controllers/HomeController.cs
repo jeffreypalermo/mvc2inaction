024bacfb20645eace5a07a6b982948228a84f193
Matt Hinze <mhinze@gmail.com>
2009-12-12 14:57:41 -0600

    simple roles example

---
+++ src/Chapter11/Roles/Roles/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Roles.Controllers
+{
+	[HandleError]
+	public class HomeController : Controller
+	{
+		public ActionResult Index()
+		{
+			ViewData["Message"] = "Welcome to ASP.NET MVC!";
+
+			return View();
+		}
+
+		[Authorize(Roles="admins")]
+		public ActionResult About()
+		{
+			return View();
+		}
+	}
+}
