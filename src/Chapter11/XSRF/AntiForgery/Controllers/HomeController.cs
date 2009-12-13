302bc4e826824cb27cfcdc8cd5dcd8345fbbb41c
Matt Hinze <mhinze@gmail.com>
2009-12-13 09:27:42 -0600

    antiforgery example

---
+++ src/Chapter11/XSRF/AntiForgery/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AntiForgery.Controllers
+{
+	[HandleError]
+	public class HomeController : Controller
+	{
+		public ActionResult Index()
+		{
+			return View();
+		}
+
+		public ActionResult About()
+		{
+			return View();
+		}
+
+		public ViewResult Save(InputModel form)
+		{
+			return View(form);
+		}
+	}
+
+	public class InputModel
+	{
+		public string Name { get; set; }
+	}
+}
---
+++ src/Chapter11/XSRF/AntiForgery/Views/Home/Save.aspx
@@ -0,0 +1,10 @@
+﻿<%@ Page Language="C#" MasterPageFile="~/Views/Shared/Site.Master" Inherits="System.Web.Mvc.ViewPage<InputModel>" %>
+<%@ Import Namespace="AntiForgery.Controllers"%>
+
+<asp:Content ID="indexTitle" ContentPlaceHolderID="TitleContent" runat="server">
+    Saved!
+</asp:Content>
+
+<asp:Content ID="indexContent" ContentPlaceHolderID="MainContent" runat="server">
+	<h1>You saved: <%= Html.Encode(Model.Name) %></h1>
+</asp:Content>
