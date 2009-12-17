7207182efeab1104da4126f6ed39ce201a71b60c
Ben Scheirman <ben@scheirman.com>
2009-12-17 00:03:17 -0600

    migrated Chapter 12 samples, added some polish & removed some funky smells

--- src/Chapter12/AjaxExamples/Global.asax.cs
+++ src/Chapter12/AjaxExamples/Global.asax.cs
@@ -1,21 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using System.Web.Routing;
 
 namespace AjaxExamples
 {
-    // Note: For instructions on enabling IIS6 or IIS7 classic mode, 
-    // visit http://go.microsoft.com/?LinkId=9394801
-
     public class MvcApplication : System.Web.HttpApplication
     {
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
             routes.IgnoreRoute("get_message.html");
+
+            routes.MapRoute("FriendlySpeakersUrl",
+                            "speakers/{urlKey}.{format}",
+                            new {controller = "Speakers", action = "details", format = "html"}
+                );
 
             routes.MapRoute(
                 "Default",                                              // Route name
