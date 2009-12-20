42be6d573f7820e827adbb81a9ab92dd7f86fa3f
Eric Hexter <eric.hexter@gmail.com>
2009-12-20 15:44:48 -0600

    Added Model State / Model Meta Data examples

---
+++ src/Chapter15/ModelState/ModelState/Global.asax.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace ModelState
+{
+    // Note: For instructions on enabling IIS6 or IIS7 classic mode, 
+    // visit http://go.microsoft.com/?LinkId=9394801
+
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        public void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                "Default",                                              // Route name
+                "{controller}/{action}/{id}",                           // URL with parameters
+                new { controller = "Home", action = "Index", id = "" }  // Parameter defaults
+            );
+
+        }
+
+        protected void Application_Start()
+        {
+            RegisterRoutes(RouteTable.Routes);
+
+            
+        }
+    }
+
+    public class ConventionProvider : DataAnnotationsModelMetadataProvider
+    {
+        protected override ModelMetadata CreateMetadata(IEnumerable<Attribute> attributes, Type containerType, Func<object> modelAccessor, Type modelType, string propertyName)
+        {
+            var meta =  base.CreateMetadata(attributes, containerType, modelAccessor, modelType, propertyName);
+            if(meta.DisplayName==null)
+                meta.DisplayName = meta.PropertyName.ToSeparatedWords() +":";
+            return meta;
+        }
+    }
+}
+
