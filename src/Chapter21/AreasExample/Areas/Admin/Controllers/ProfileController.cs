30c1200e3699c248f439de8b91ba7e2ebd1a226b
Jimmy Bogard <jimmy.bogard@gmail.com>
2010-01-03 15:13:35 -0600

    Adding chapter 21 example on areas

---
+++ src/Chapter21/AreasExample/Areas/Admin/Controllers/ProfileController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AreasExample.Areas.Admin.Models;
+
+namespace AreasExample.Areas.Admin.Controllers
+{
+    public class ProfileController : Controller
+    {
+		private readonly IProfileRepository _profileRepository;
+
+		public ProfileController(IProfileRepository profileRepository)
+		{
+			_profileRepository = profileRepository;
+		}
+
+		public ProfileController() : this(new ProfileRepository()) { }
+
+
+        public ActionResult Index()
+        {
+			var profiles = _profileRepository.GetAll();
+			return View(profiles);
+		}
+
+		public ViewResult Show(string username)
+		{
+			var profile = _profileRepository.Find(username);
+			if (profile == null)
+			{
+				profile = new Profile(username);
+				_profileRepository.Add(profile);
+			}
+
+			bool hasPermission = User.Identity.Name == username;
+
+			ViewData["hasPermission"] = hasPermission;
+
+			return View(profile);
+		}
+
+		public ViewResult Edit(string username)
+		{
+			var profile = _profileRepository.Find(username);
+			return View(new EditProfileInput(profile));
+		}
+    }
+}
---
+++ src/Chapter21/AreasExample/Areas/Admin/Views/Profile/Edit.aspx
@@ -0,0 +1,25 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Views/Shared/Site.Master" Inherits="System.Web.Mvc.ViewPage<EditProfileInput>" %>
+<%@ Import Namespace="AreasExample.Areas.Admin.Models"%>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="TitleContent" runat="server">
+	Edit
+</asp:Content>
+
+<asp:Content ID="Content2" ContentPlaceHolderID="MainContent" runat="server">
+
+    <h2>Edit</h2>
+
+
+    <% using (Html.BeginForm("Save", "Profile")) {%>
+    
+		<%= Html.EditorForModel() %>
+        <p>
+			<input type="submit" value="Save" name="SaveButton" />
+        </p>
+
+    <% } %>
+
+    <div>
+        <%=Html.ActionLink("Back to List", "Index") %>
+    </div>
+</asp:Content>
