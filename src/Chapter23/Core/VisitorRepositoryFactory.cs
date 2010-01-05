e2e502cdaad7992bcff78c77f587cbc4458501ef
jeffreypalermo <jeffreypalermo@yahoo.com>
2010-01-05 00:37:27 -0600

    - Finished code samples, listing and reshot screenshots for chapter 23. - Chapter 23's manuscript is complete and ready for review.

--- src/Chapter23/Core/VisitorRepositoryFactory.cs
+++ src/Chapter23/Core/VisitorRepositoryFactory.cs
@@ -2,23 +2,23 @@ using System;
 
 namespace Core
 {
-	public class VisitorRepositoryFactory
-	{
-		//meant to be set on application start
-		public static Func<IVisitorRepository> RepositoryBuilder = 
-			CreateDefaultRepositoryBuilder;
+    public class VisitorRepositoryFactory
+    {
+        //meant to be set on application start
+        public static Func<IVisitorRepository> RepositoryBuilder =
+            CreateDefaultRepositoryBuilder;
 
-		private static IVisitorRepository CreateDefaultRepositoryBuilder()
-		{
-			//throw if factory not initialized
-			throw new Exception("No repository builder specified.");
-		}
+        private static IVisitorRepository CreateDefaultRepositoryBuilder()
+        {
+            //throw if factory not initialized
+            throw new Exception("No repository builder specified.");
+        }
 
-		public IVisitorRepository BuildRepository()
-		{
-			//Uses the Func<IVisitorRepository> to build the instance
-			IVisitorRepository repository = RepositoryBuilder();
-			return repository;
-		}
-	}
+        public IVisitorRepository BuildRepository()
+        {
+            //Uses the Func<IVisitorRepository> to build the instance
+            IVisitorRepository repository = RepositoryBuilder();
+            return repository;
+        }
+    }
 }
--- src/Chapter23/IntegrationTests/DatabaseTester.cs
+++ src/Chapter23/IntegrationTests/DatabaseTester.cs
@@ -4,14 +4,15 @@ using NUnit.Framework;
 
 namespace IntegrationTests
 {
-	[TestFixture]
-	public class DatabaseTester
-	{
-		[Test, Explicit]
-		public void CreateDatabaseSchmea()
-		{
-			var export = new SchemaExport(DataConfig.BuildConfiguration());
-			export.Execute(true, true, false);
-		}
-	}
+    [TestFixture]
+    public class DatabaseTester
+    {
+        [Test, Explicit]
+        public void CreateDatabaseSchmea()
+        {
+            var export = new SchemaExport(
+                DataConfig.BuildConfiguration());
+            export.Execute(true, true, false);
+        }
+    }
 }
