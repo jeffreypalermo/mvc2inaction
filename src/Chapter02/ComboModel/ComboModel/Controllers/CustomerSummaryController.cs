5441e272f0eff5785ba1b27d5fc3812596ebb658
Matt Hinze <mhinze@gmail.com>
2010-01-06 21:47:05 -0600

    rough draft presentation model

--- src/Chapter02/ComboModel/ComboModel/Controllers/CustomerSummaryController.cs
+++ src/Chapter02/ComboModel/ComboModel/Controllers/CustomerSummaryController.cs
@@ -4,61 +4,59 @@ using ComboModel.Models;
 
 namespace ComboModel.Controllers
 {
-	public class CustomerSummaryController : Controller
-	{
-		public ViewResult Index()
-		{
-			var customerSummaries = new CustomerSummaries();
+    public class CustomerSummaryController : Controller
+    {
+        private readonly CustomerSummaries _customerSummaries = new CustomerSummaries();
 
-			IEnumerable<CustomerSummary> summaries =
-				customerSummaries.GetAll();
+        public ViewResult Index()
+        {
+            IEnumerable<CustomerSummary> summaries =
+                _customerSummaries.GetAll();
 
-			return View(summaries);
-		}
+            return View(summaries);
+        }
 
-		public ViewResult Save(List<CustomerSummaryInput> input)
-		{
-			return View(input);
-		}
-	}
+        public ViewResult Save
+            (List<CustomerSummary.CustomerSummaryInput> input)
+        {
+            return View(input);
+        }
+    }
 
-	public class CustomerSummaries
-	{
-		public IEnumerable<CustomerSummary> GetAll()
-		{
-			return new[]
-			       	{
-			       		new CustomerSummary
-			       			{
-			       				Active = true,
-			       				FirstName = "John",
-			       				LastName = "Smith",
-			       				MostRecentOrderDate = "02/07/10",
-			       				Number = 4,
-			       				OrderCount = "42",
-			       				ServiceLevel = "Standard"
-			       			},
-			       		new CustomerSummary
-			       			{
-			       				Active = false,
-			       				FirstName = "Susan",
-			       				LastName = "Power",
-			       				MostRecentOrderDate = "02/02/10",
-			       				Number = 5,
-			       				OrderCount = "1",
-			       				ServiceLevel = "Standard"
-			       			},
-			       		new CustomerSummary
-			       			{
-			       				Active = true,
-			       				FirstName = "Jim",
-			       				LastName = "Doe",
-			       				MostRecentOrderDate = "02/09/10",
-			       				Number = 6,
-			       				OrderCount = "7",
-			       				ServiceLevel = "Premier"
-			       			},
-			       	};
-		}
-	}
+    public class CustomerSummaries
+    {
+        public IEnumerable<CustomerSummary> GetAll()
+        {
+            return new[]
+                       {
+                           new CustomerSummary
+                               {
+                                   Input = new CustomerSummary.CustomerSummaryInput {Active = true, Number = 4},
+                                   FirstName = "John",
+                                   LastName = "Smith",
+                                   MostRecentOrderDate = "02/07/10",
+                                   OrderCount = "42",
+                                   ServiceLevel = "Standard"
+                               },
+                           new CustomerSummary
+                               {
+                                   Input = new CustomerSummary.CustomerSummaryInput {Active = false, Number = 5},
+                                   FirstName = "Susan",
+                                   LastName = "Power",
+                                   MostRecentOrderDate = "02/02/10",
+                                   OrderCount = "1",
+                                   ServiceLevel = "Standard"
+                               },
+                           new CustomerSummary
+                               {
+                                   Input = new CustomerSummary.CustomerSummaryInput {Active = true, Number = 6},
+                                   FirstName = "Jim",
+                                   LastName = "Doe",
+                                   MostRecentOrderDate = "02/09/10",
+                                   OrderCount = "7",
+                                   ServiceLevel = "Premier"
+                               },
+                       };
+        }
+    }
 }
--- src/Chapter02/ComboModel/ComboModel/Models/CustomerSummary.cs
+++ src/Chapter02/ComboModel/ComboModel/Models/CustomerSummary.cs
@@ -1,13 +1,19 @@
 namespace ComboModel.Models
 {
-	public class CustomerSummary
-	{
-		public string FirstName { get; set; }
-		public string LastName { get; set; }
-		public int Number { get; set; }
-		public bool Active { get; set; }
-		public string ServiceLevel { get; set; }
-		public string OrderCount { get; set; }
-		public string MostRecentOrderDate { get; set; }
-	}
+public class CustomerSummary
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string ServiceLevel { get; set; }
+    public string OrderCount { get; set; }
+    public string MostRecentOrderDate { get; set; }
+
+    public CustomerSummaryInput Input { get; set; }
+
+    public class CustomerSummaryInput
+    {
+        public int Number { get; set; }
+        public bool Active { get; set; }
+    }
+}
 }
