c9ad2d0e49db709c7af6f3f261bd951bd4195784
Matt Hinze <mhinze@gmail.com>
2010-01-26 20:02:58 -0600

    finished draft of automapper chapter

--- src/Chapter18/WithAutomapper/Models/NameFormatter.cs
+++ src/Chapter18/WithAutomapper/Models/NameFormatter.cs
@@ -3,35 +3,35 @@ using Core.Model;
 
 namespace WithAutomapper.Models
 {
-	public class NameFormatter : BaseFormatter<Name>
+public class NameFormatter : BaseFormatter<Name>
+{
+	protected override string FormatValueCore(Name value)
 	{
-		protected override string FormatValueCore(Name value)
+		var sb = new StringBuilder();
+
+		if (value == null) return string.Empty;
+
+		if (!string.IsNullOrEmpty(value.First))
 		{
-			var sb = new StringBuilder();
-
-			if (value == null) return string.Empty;
-
-			if (!string.IsNullOrEmpty(value.First))
-			{
-				sb.Append(value.First);
-			}
-
-			if (!string.IsNullOrEmpty(value.Middle))
-			{
-				sb.Append(" " + value.Middle);
-			}
-
-			if (!string.IsNullOrEmpty(value.Last))
-			{
-				sb.Append(" " + value.Last);
-			}
-
-			if (value.Suffix != null)
-			{
-				sb.Append(", " + value.Suffix.DisplayName);
-			}
-
-			return sb.ToString();
+			sb.Append(value.First);
 		}
+
+		if (!string.IsNullOrEmpty(value.Middle))
+		{
+			sb.Append(" " + value.Middle);
+		}
+
+		if (!string.IsNullOrEmpty(value.Last))
+		{
+			sb.Append(" " + value.Last);
+		}
+
+		if (value.Suffix != null)
+		{
+			sb.Append(", " + value.Suffix.DisplayName);
+		}
+
+		return sb.ToString();
 	}
+}
 }
