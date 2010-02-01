30da4eec32d867ae154b591cf7b3b6a4f87a25e4
Jimmy Bogard <jimmy.bogard@gmail.com>
2010-01-31 18:51:38 -0600

    Finishing ch. 14 example and manuscript

--- src/Chapter14/ValueProviders/Global.asax.cs
+++ src/Chapter14/ValueProviders/Global.asax.cs
@@ -6,44 +6,29 @@ using ValueProvidersExample.Helpers;
 
 namespace ValueProvidersExample
 {
-	// Note: For instructions on enabling IIS6 or IIS7 classic mode, 
-	// visit http://go.microsoft.com/?LinkId=9394801
+    // Note: For instructions on enabling IIS6 or IIS7 classic mode, 
+    // visit http://go.microsoft.com/?LinkId=9394801
 
-	public class MvcApplication : System.Web.HttpApplication
-	{
-		public static void RegisterRoutes(RouteCollection routes)
-		{
-			routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
-			routes.MapRoute(
-				"Default",                                              // Route name
-				"{controller}/{action}/{id}",                           // URL with parameters
-				new { controller = "Home", action = "Index", id = "" }  // Parameter defaults
-			);
+            routes.MapRoute(
+                "Default",                                              // Route name
+                "{controller}/{action}/{id}",                           // URL with parameters
+                new { controller = "Home", action = "Index", id = "" }  // Parameter defaults
+            );
 
-		}
+        }
 
-		protected void Application_Start()
-		{
-			AreaRegistration.RegisterAllAreas();
-            ValueProviderFactories.Factories.Add(new ExplicitFactory(new SessionValueProvider()));
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            ValueProviderFactories.Factories.Add(new SessionValueProviderFactory());
 
-			RegisterRoutes(RouteTable.Routes);
-		}
-
-	    public class ExplicitFactory : ValueProviderFactory
-	    {
-	        private readonly IValueProvider _valueProvider;
-
-	        public ExplicitFactory(IValueProvider valueProvider)
-	        {
-	            _valueProvider = valueProvider;
-	        }
-
-	        public override IValueProvider GetValueProvider(ControllerContext controllerContext)
-	        {
-	            return _valueProvider;
-	        }
-	    }
-	}
+            RegisterRoutes(RouteTable.Routes);
+        }
+    }
 }
--- src/Chapter14/ValueProviders/Helpers/SessionValueProvider.cs
+++ src/Chapter14/ValueProviders/Helpers/SessionValueProvider.cs
@@ -1,40 +1,60 @@
+using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.Globalization;
 using System.Web;
 using System.Web.Mvc;
+using System.Linq;
 
 namespace ValueProvidersExample.Helpers
 {
-    public class SessionValueProvider : IValueProvider
+public class SessionValueProvider : IValueProvider
+{
+    public SessionValueProvider(HttpSessionStateBase session)
     {
-        private readonly HttpSessionStateBase _session;
+        AddValues(session);
+    }
 
-        public SessionValueProvider(HttpSessionStateBase session)
+private readonly HashSet<string> _prefixes
+    = new HashSet<string>(StringComparer.OrdinalIgnoreCase);
+private readonly Dictionary<string, ValueProviderResult> _values
+    = new Dictionary<string, ValueProviderResult>(StringComparer.OrdinalIgnoreCase);
+
+private void AddValues(HttpSessionStateBase session)
+{
+    if (session.Keys.Count > 0)
+    {
+        _prefixes.Add("");
+    }
+
+    foreach (string key in session.Keys)
+    {
+        if (key != null)
         {
-            _session = session;
+            _prefixes.Add(key);
+
+            object rawValue = session[key];
+            string attemptedValue = session[key].ToString();
+            _values[key] = new ValueProviderResult(
+                rawValue, 
+                attemptedValue, 
+                CultureInfo.CurrentCulture);
         }
+    }
+}
 
-        public SessionValueProvider() : this(new HttpContextWrapper(HttpContext.Current).Session)
-        {
-        }
+public bool ContainsPrefix(string prefix)
+{
+    return _prefixes.Contains(prefix);
+}
 
-        public const string CurrentUserKey = "CurrentUser";
-
-        public bool ContainsPrefix(string prefix)
-        {
-            return prefix == CurrentUserKey;
-        }
-
-        public ValueProviderResult GetValue(string key)
-        {
-            if (key != CurrentUserKey)
-                return null;
-
-            object currentUserInSession = _session[CurrentUserKey];
-
-            if (currentUserInSession == null)
-                return null;
-
-            return new ValueProviderResult(currentUserInSession, CurrentUserKey, CultureInfo.InvariantCulture);
-        }
+public ValueProviderResult GetValue(string key)
+{
+    ValueProviderResult result;
+    
+    _values.TryGetValue(key, out result);
+    
+    return result;
+}
     }
 }
