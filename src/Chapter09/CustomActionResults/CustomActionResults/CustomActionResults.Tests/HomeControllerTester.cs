0c556fbc878d0c9fb3f986824a4a5f75a5ad129b
Eric Hexter <eric.hexter@gmail.com>
2010-02-26 00:05:32 -0600

    added the actual files. chapter 9

---
+++ src/Chapter09/CustomActionResults/CustomActionResults/CustomActionResults.Tests/HomeControllerTester.cs
@@ -0,0 +1,18 @@
+using CustomActionResults.Controllers;
+using NUnit.Framework;
+using MvcContrib.TestHelper;
+namespace CustomActionResults.Tests
+{
+    [TestFixture]
+    public class HomeControllerTester
+    {
+        [Test]
+        public void The_Logout_should_redirect_to_the_index_after_logout()
+        {
+            var controller = new HomeController();
+            LogoutActionResult result = (LogoutActionResult) controller.Logout();
+            result.ActionAfterLogout.AssertActionRedirect().ToAction<HomeController>(c => c.Index());
+        }
+        
+    }
+}
