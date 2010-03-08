483b19556840f672695cf54657a7d1a9e54b6c9e
Jimmy Bogard <jimmy.bogard@gmail.com>
2010-03-07 21:07:11 -0600

    Minor styling fixes for code snippets

--- src/Chapter15/ModelState/ModelState/Global.asax.cs
+++ src/Chapter15/ModelState/ModelState/Global.asax.cs
@@ -28,7 +28,7 @@ namespace ModelState
         {
             RegisterRoutes(RouteTable.Routes);
 
-            
+
         }
     }
 
@@ -36,9 +36,9 @@ namespace ModelState
     {
         protected override ModelMetadata CreateMetadata(IEnumerable<Attribute> attributes, Type containerType, Func<object> modelAccessor, Type modelType, string propertyName)
         {
-            var meta =  base.CreateMetadata(attributes, containerType, modelAccessor, modelType, propertyName);
-            if(meta.DisplayName==null)
-                meta.DisplayName = meta.PropertyName.ToSeparatedWords() +":";
+            var meta = base.CreateMetadata(attributes, containerType, modelAccessor, modelType, propertyName);
+            if (meta.DisplayName == null)
+                meta.DisplayName = meta.PropertyName.ToSeparatedWords() + ":";
             return meta;
         }
     }
